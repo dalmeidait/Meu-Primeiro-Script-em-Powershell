ee6d1665f780bede425a3f7813d4f0bad7a2099d
Daniel Barros Almeida <d.almeid@outlook.com>
2025-09-16 16:47:41 -0300

    Adição de script

---
+++ Exibe pastas.ps1
@@ -0,0 +1,5 @@
+﻿# Apenas um teste inicial
+cls
+Write-Host "Exibindo documentos"
+Write-Host "Meu primeiro script básico em Powershell que mostra pastas dos Documentos do perfil público"
+Get-ChildItem c:\Users\Public\Documents\
